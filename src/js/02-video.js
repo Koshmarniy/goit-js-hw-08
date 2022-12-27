07b41ad9359f2707b405b8250566e0eca66e44dc
Koshmarniy <110358601+Koshmarniy@users.noreply.github.com>
2022-12-27 20:51:39 +0200

    add task№2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,11 +1,32 @@
 import Player from '@vimeo/player';
 // console.log(Player)
-// import { throttle } from 'lodash';
+import throttle from 'lodash.throttle';
+// const iframe = document.querySelector('iframe');
+// const player = new Player(iframe);
+// player.on('play', function() {
+//     console.log('played the video!');
+// });
+
+const throttle = require('lodash.throttle');
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-player.on('play', function() {
-    console.log('played the video!');
-});
+
+const KEY = "videoplayer-current-time"
+
+player.on('timeupdate', throttle(onPlay, 1000));
+    
+function onPlay(data) {
+    const current = localStorage.setItem(KEY, data.seconds)
+}
+
+const result = localStorage.getItem(KEY)
+
+if (result) {
+player.setCurrentTime(result)
+} else {
+    return
+}
+console.log(throttle)
 
 
 
